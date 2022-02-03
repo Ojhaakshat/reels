7acdbc30908f99a9e00f5404f1aa5051411399fd
Ojhaakshat <ojhaakshat123@gmail.com>
2022-02-04 00:04:50 +0530

    uploads bugs fixed

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,19 +1,30 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../Context/AuthContext';
+import { database } from '../firebase';
 import Uploads from './Uploads'; 
 
 export default function Feed() {
+
+    const [userData, setUserData] = useState(''); 
     const {logout} = useContext(AuthContext);
     const {user} = useContext(AuthContext);
     const navigate = useNavigate();
     console.log(user);
+
+    useEffect(()=> {
+        const unsub = database.users.doc(user.uid).onSnapshot((snapshot) => {
+            setUserData(snapshot.data())
+
+        })
+        return () => {unsub()}
+    }, [user])
 
     return (<div style={{display:'flex', justifyContent: 'center',alignItems: 'center', flexDirection: 'column'}}>
                 <div className='comp' style={{width:'50%'}}>
                     <h1>Feeds</h1>
                     <button onClick={logout}>Log out</button>
                 </div>
-                <Uploads user={user}/>
+                <Uploads user={userData}/>
             </div>);
 }
